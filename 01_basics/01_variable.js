7d68bf0c4dbee55413860ebe59e78066c6eaad5f
Ravi <ravikashyapwwe20@gmail.com>
2025-09-08 12:01:10 +0530

    8th sep learning

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountID=12345;
+let accountName="John Doe";
+var accountBalance=1000.50;
+accountcity="New York";
+// accountID=12; // not allowed
+console.log(accountID);
+accountBalance=2000.75; // allowed
+accountName="Jane Doe"; // allowed
+let accountcountry; // so if we do not assign any value it will be undefined
+console.table([accountID,accountName,accountBalance,accountcity,accountcountry]);
+
+/*
+prefer not to use var because of issues with block scope and functional scope
+*/
