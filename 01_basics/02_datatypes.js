440ccf4ef6d388e8f650c709fa17dffd59140cf9
Ravi <ravikashyapwwe20@gmail.com>
2025-09-09 11:14:32 +0530

    datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+ "use strict"; // treat all js code as modern js
+// alert("hello world");// we can use alert, prompt, confirm in browser only not in nodejs
+console.log("hello world");
+console.log(3 + 4);
+//datatypes
+//1. primitive datatypes: number, string, boolean, null, undefined, symbol
+//2. reference datatypes: object, array, function
+let name = "John"; // string
+let age = 25; // number
+let isMarried = false; // boolean
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isMarried);
+let city=null;
+console.log(typeof city); // object
+let state; // undefined
+console.log(typeof state);//undefined
+
